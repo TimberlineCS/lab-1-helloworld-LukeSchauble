e6985cef0d448e71313bafbc14350c26aac08374
LukeSchauble <22luke.schauble@boiseschools.net>
2020-08-20 16:43:15 +0000

    Started Class file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+//Luke Schauble
+//AP Computer Science
+//August 20, 2020
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.print("Hello");
+    System.out.print("");
+
+  }
+
+}
