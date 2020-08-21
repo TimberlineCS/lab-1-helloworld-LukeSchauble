44f2a07536c2efcf63aaf1e93fedda3999d3172c
LukeSchauble <22luke.schauble@boiseschools.net>
2020-08-21 16:44:41 +0000

    finished

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,16 +4,16 @@
 public class HelloWorld{
   public static void main(String args[]){
     System.out.println("      .");
-  //  System.out.println("   __/ \__");
-  //  System.out.println("   \     /");
-  //  System.out.println("   /.'0'.\");
+    System.out.println("   __/ \\__");
+    System.out.println("   \\     /");
+    System.out.println("    /.'0'.\\");
     System.out.println("    .0.'.");
     System.out.println("   .'.'0'.");
     System.out.println("  0'.0.'.0.");
     System.out.println(" .'.0.'.0.'.");
     System.out.println(".0.'.0.'.0.'.");
     System.out.println("   [_____]");
-  //  System.out.println("    \___/");
+    System.out.println("    \\___/");
    /* I have made a Christmas Tree!*/ 
   /* The lines of code that I have commented were errors and I didn't know how to fix them. 7,8, and 16 were illegal escape characters and 9 was an unclosed string literal.*/
   }
