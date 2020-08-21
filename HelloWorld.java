94754477fc2f2c5e84b387d51fef8dae649f7b8a
LukeSchauble <22luke.schauble@boiseschools.net>
2020-08-21 16:37:44 +0000

    Finished with errors

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,18 @@
 //August 20, 2020
 public class HelloWorld{
   public static void main(String args[]){
-    System.out.print("Hello");
-    System.out.print("");
-
+    System.out.println("      .");
+  //  System.out.println("   __/ \__");
+  //  System.out.println("   \     /");
+  //  System.out.println("   /.'0'.\");
+    System.out.println("    .0.'.");
+    System.out.println("   .'.'0'.");
+    System.out.println("  0'.0.'.0.");
+    System.out.println(" .'.0.'.0.'.");
+    System.out.println(".0.'.0.'.0.'.");
+    System.out.println("   [_____]");
+  //  System.out.println("    \___/");
+   /* I have made a Christmas Tree!*/ 
+  /* The lines of code that I have commented were errors and I didn't know how to fix them. 7,8, and 16 were illegal escape characters and 9 was an unclosed string literal.*/
   }
-
 }
